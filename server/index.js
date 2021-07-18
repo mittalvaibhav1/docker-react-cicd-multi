1cb03a145ff2b2c107bf086d69ba4a97763a2ea6
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-07-18 12:53:23 +0530

    typo

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ app.get('/', (req, res) => {
     res.send('Hi');
 });
 
-app.get('/vales/all', async (req, res) => {
+app.get('/values/all', async (req, res) => {
     const values = await pgClient.query('SELECT * from values');
     res.send(values.rows);
 });
